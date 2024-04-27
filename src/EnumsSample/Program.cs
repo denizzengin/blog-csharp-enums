5ae570dad63a9f6364631502f583c57b4af83d76
DZ <deniz.zengin@qinshift.com>
2024-04-27 23:11:01 +0200

    feat: add enum sample for blog-post.

---
+++ src/EnumsSample/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EnumsSample;
+
+Console.WriteLine("Hello, World!");
+
+StudentType studentTypeNone = StudentType.Unknown;
+int studentTypeNoneUnderlyingValue = (int)studentTypeNone;
+
+StudentType studentTypePrimary = StudentType.Primary;
+StudentType studentTypeCollege = StudentType.College;
+
+StudentType groupedType =  studentTypeNone | studentTypePrimary | studentTypeCollege ;
+Console.WriteLine($"{groupedType}");
+
+Console.WriteLine(groupedType.HasFlag(StudentType.Univertsity));
+
+Console.WriteLine($"{studentTypeNone:d} {studentTypePrimary:d} {studentTypeCollege:d}");
+
+Console.ReadLine();
---
+++ src/EnumsSample/StudentType.cs
@@ -0,0 +1,10 @@
+﻿namespace EnumsSample;
+
+[Flags]
+public enum StudentType : byte
+{
+    Unknown = 1,
+    Primary = 1 << 1,
+    College = 1 << 2,
+    Univertsity = 1 << 3,
+}
